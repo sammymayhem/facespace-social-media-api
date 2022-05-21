3ad86a60717a7b6a921d3c0c86f3e515b349a617
Sam Wallace <sam@sammymayhem.com>
2022-05-21 10:41:55 -0700

    added thoughtController and thoughtRoutes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction,
+} = require('../../controllers/thoughtController');
+
+// /api/thoughts
+router.route('/').get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router
+    .route('/:thoughtId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reaction
+router.route('/:thoughtId/tags').post(addReaction);
+
+// /api/thoughts/:thoughtId/reaction/:reactionId
+router.route('/:thoughtId/reaction/:reactionId').delete(removeReaction);
+
+module.exports = router;
